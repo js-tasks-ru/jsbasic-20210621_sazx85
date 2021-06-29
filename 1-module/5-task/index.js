7041e21aed39ff897fb112d4cc28ddb3ab02c023
Eugene Stashenin <sazx85@gmail.com>
2021-06-29 08:58:13 +0300

    fixes for 1 module tasks 2 and 5

--- 1-module/5-task/index.js
+++ 1-module/5-task/index.js
@@ -1,7 +1,7 @@
 function truncate(str, maxlength) {
   if (str.length <= maxlength) {
     return str;
-  } else {
-    return str.slice(0, maxlength - 1) + '…';
   }
+
+  return str.slice(0, maxlength - 1) + '…';
 }
