29abd9096fbc553a99e3e17a01d7df292fd02a76
Eugene Stashenin <sazx85@gmail.com>
2021-07-06 20:05:52 +0300

    3 module 4th task

--- 3-module/4-task/index.js
+++ 3-module/4-task/index.js
@@ -1,3 +1,11 @@
-function showSalary(users, age) {
-  // ваш код...
+function showSalary(arr, age) {
+  let str = [];
+
+  for (let value of arr) {
+    if ( value.age <= age ) {
+      str.push(`${value.name}, ${value.balance}`);
+    }
+  }
+
+  return str.join("\n");
 }
