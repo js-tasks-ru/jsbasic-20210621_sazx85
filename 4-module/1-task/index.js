18ae06660f801b92de6439f3ab64afab3a82352d
Eugene Stashenin <sazx85@gmail.com>
2021-07-09 18:52:25 +0300

    4th module 1st task

--- 4-module/1-task/index.js
+++ 4-module/1-task/index.js
@@ -1,3 +1,11 @@
 function makeFriendsList(friends) {
-  // ваш код...
+  let list = document.createElement('ul');
+
+  for (let item of friends) {
+    let listItem = document.createElement('li');
+    listItem.innerHTML = `${item.firstName} ${item.lastName}`;
+    list.append(listItem);
+  }
+
+  return list;
 }
