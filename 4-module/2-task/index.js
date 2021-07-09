41534b43c8d28de63aa29f710e3b99ac6b26692a
Eugene Stashenin <sazx85@gmail.com>
2021-07-09 19:24:33 +0300

    4th module 2nd task

--- 4-module/2-task/index.js
+++ 4-module/2-task/index.js
@@ -1,3 +1,7 @@
 function makeDiagonalRed(table) {
-  // ваш код...
+  let tr = table.querySelectorAll('tr');
+
+  for (let i = 0; i < tr.length; i++) {
+    table.rows[i].cells[i].style.backgroundColor = 'red';
+  }
 }
