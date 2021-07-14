e04dabedcb012894c491ee0fa1afa7983058f53f
Eugene Stashenin <sazx85@gmail.com>
2021-07-14 19:22:59 +0300

    5th module 3rd task

--- 5-module/3-task/index.js
+++ 5-module/3-task/index.js
@@ -1,3 +1,31 @@
 function initCarousel() {
-  // ваш код...
+  const carousel = document.querySelector("[data-carousel-holder]");
+  const sliderLength = carousel.querySelectorAll(".carousel__slide");
+  const carouselSwiper = carousel.querySelector(".carousel__inner");
+  const arrowLeft = carousel.querySelector(".carousel__arrow_left");
+  const arrowRight = carousel.querySelector(".carousel__arrow_right");
+  let currentOffset = 0;
+
+  arrowLeft.style.display = 'none';
+
+  arrowLeft.addEventListener('click', function () {
+    arrowRight.style.display = '';
+    currentOffset -= carouselSwiper.offsetWidth;
+    carouselSwiper.style.transform = 'translateX(-' + currentOffset + 'px)';
+
+    if (currentOffset === 0) {
+      this.style.display = 'none';
+    }
+  });
+
+  arrowRight.addEventListener('click', function () {
+    arrowLeft.style.display = '';
+    currentOffset += carouselSwiper.offsetWidth;
+    carouselSwiper.style.transform = 'translateX(-' + currentOffset + 'px)';
+
+    if (currentOffset === (sliderLength.length - 1) * carouselSwiper.offsetWidth) {
+      this.style.display = 'none';
+    }
+  });
+
 }
