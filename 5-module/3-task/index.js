844fef6243e0d7e8eebd1846ada6f244e3e30d9b
Eugene Stashenin <sazx85@gmail.com>
2021-07-16 20:08:42 +0300

    fixes 5th module 3rd task

--- 5-module/3-task/index.js
+++ 5-module/3-task/index.js
@@ -11,7 +11,7 @@ function initCarousel() {
   arrowLeft.addEventListener('click', function () {
     arrowRight.style.display = '';
     currentOffset -= carouselSwiper.offsetWidth;
-    carouselSwiper.style.transform = 'translateX(-' + currentOffset + 'px)';
+    carouselSwiper.style.transform = `translateX(-${currentOffset}px)`;
 
     if (currentOffset === 0) {
       this.style.display = 'none';
@@ -21,7 +21,7 @@ function initCarousel() {
   arrowRight.addEventListener('click', function () {
     arrowLeft.style.display = '';
     currentOffset += carouselSwiper.offsetWidth;
-    carouselSwiper.style.transform = 'translateX(-' + currentOffset + 'px)';
+    carouselSwiper.style.transform = `translateX(-${currentOffset}px)`;
 
     if (currentOffset === (sliderLength.length - 1) * carouselSwiper.offsetWidth) {
       this.style.display = 'none';
